1be11302089c586fbb045a88e4ae4a40f616528d
Aziz Becha <aziz07becha@gmail.com>
2024-02-16 11:37:31 +0100

    added used font and main color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,30 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      main: "#BB2649",
+      black: "#181818",
+      white: "#fff"
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+      fontWeight: {
+        'light': '300',
+        'regular': '400',
+        'medium': '500',
+        'bold': '700',
+      },
+      fontStyle: {
+        'light-italic': 'italic',
+        'regular-italic': 'italic',
+        'medium-italic': 'italic',
+        'bold-italic': 'italic',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
