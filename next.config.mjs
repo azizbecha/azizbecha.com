b4319c7dc16c24ebd35e7e7e17778f3769719710
Aziz Becha <aziz07becha@gmail.com>
2024-03-16 17:20:20 +0000

    bugz

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
