56fd0b2411b09d16834af7577ca357ab6b81f289
Aziz Becha <aziz07becha@gmail.com>
2024-06-26 06:02:17 +0100

    added specific post fetching api

---
+++ app/api/posts/[slug]/route.ts
@@ -0,0 +1,13 @@
+import { getPostBySlug } from '@/lib/api';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+    const { slug } = params;
+    const post = getPostBySlug(slug);
+
+    if (!post) {
+        return NextResponse.json({ status: 404, error: 'Post not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ status: 200, post });
+}
