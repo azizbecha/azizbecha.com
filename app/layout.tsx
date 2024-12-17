91091492b1cdde68e34d944135c13829617cd74b
Aziz Becha <aziz07becha@gmail.com>
2024-12-17 01:39:34 +0100

    removed custom animated cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 
 import { Toaster } from "react-hot-toast";
 
-import AnimatedCursor from "react-animated-cursor"
-
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AOSInit from "@/components/AOSInit";
@@ -58,20 +56,6 @@ export default function RootLayout({
         <AOSInit />
         <ProgressBar />
         <ScrollToTopButton />
-        <AnimatedCursor
-          innerSize={8}
-          outerSize={35}
-          innerScale={1}
-          outerScale={2}
-          outerAlpha={0}
-          trailingSpeed={3}
-          innerStyle={{
-            backgroundColor: "#BB2649"
-          }}
-          outerStyle={{
-            border: '3px solid #fff'
-          }}
-        />
         {children}
         <Footer />
       </body>
